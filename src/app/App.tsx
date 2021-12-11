20945f20f132031a90c4aa2e8c64d006b15582c3
Bea <you@example.com>
2021-12-11 15:36:36 +0600

    login form

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SignInForm } from '../profile';
+import { Layout } from './layout';
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        <SignInForm />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {makeStyles} from '@mui/styles'
+
+import Navbar from './Navber'
+
+
+type Props = {
+  children: React.ReactNode
+}
+
+export const Layout = ({children}: Props) => {
+  const styles = useStyles()
+
+  return (
+    <main>
+      <Navbar />
+      <div className={styles.pageContent}>
+        {children}
+      </div>
+    </main>
+  )
+}
+
+const useStyles = makeStyles({
+  pageContent: {
+    paddingTop: '100px',
+  },
+})
